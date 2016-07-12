92fa3ef452f846384944aab30fabd62b355eb8a7
Jinsu Mathew <jinsu@askme.in>
2016-07-12 13:48:49 -0500

    Login to admin dashboard with env config check. Ref #12

---
+++ controllers/dashboard/access.js
@@ -0,0 +1,50 @@
+/**
+ * Dashboard access management
+ */
+
+var debug = require('debug')('sumoqs:server');
+
+/**
+ * @method login
+ */
+exports.login = function(req, res) {
+  res.render('dashboard/login', {
+    title: 'SumoQs Dashboard - Login'
+  });
+};
+
+
+/**
+ * @method verify
+ */
+exports.verify = function(req, res) {
+
+  // Get password from config env
+  var configPassword = process.env.ADMIN_PASS;
+
+  // If config not set by env
+  if (!configPassword) {
+    return res.render('error', {
+      message: 'Application not configured with a admin password.',
+      error: new Error('Invalid application config: ADMIN_PASS env missing')
+    });
+  }
+
+  var isCorrect = configPassword === req.body.password
+    ? true
+    : false;
+  debug(isCorrect);
+  if (isCorrect) {
+    res.cookie('isVerified', isCorrect, {
+      httpOnly: true,
+      signed: true
+    });
+  } else {
+    return res.render('dashboard/login', {
+      title: 'SumoQs Dashboard - Login failed',
+      err: 'Invalid password'
+    });
+  }
+
+  res.redirect('/dashboard');
+};
--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,14 +1,17 @@
 var express = require('express');
 var router = express.Router();
 var dashboard = require(__base + 'controllers/dashboard');
+var access = require(__base + 'controllers/dashboard/access');
 
 /* Dashboard routes */
 router.get('/', dashboard.listSurveys);
+router.get('/login', access.login);
 router.get('/viewoptions/:surveyId', dashboard.listChoices);
 router.get('/stats/:surveyId', dashboard.getStats);
 router.post('/addsurvey', dashboard.addSurvey);
 router.post('/addchoice', dashboard.addChoice);
 router.post('/deletesurvey', dashboard.deleteSurvey);
+router.post('/login', access.verify);
 
 
 module.exports = router;
