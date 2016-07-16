b67f31ed62cbfe705b2de3e1be1fcfa2c802fdf2
Jinsu Mathew <jinsu@askme.in>
2016-07-16 09:56:55 -0500

    Removes unused data models

--- routes/users.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var users = require(__base + 'controllers/users');
-
-/* GET users listing. */
-router.get('/', users.create);
-
-module.exports = router;
